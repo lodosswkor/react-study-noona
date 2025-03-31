a5d44507d046205462d12a4eb24201003ec36ffd
lodossw <lodossw@gmail.com>
2025-03-31 22:12:02 +0900

    CI 테스트

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          디벨롭 브랜치에서 푸시
         </a>
       </header>
     </div>
