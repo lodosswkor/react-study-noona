9736b3971c143cb12a319b1b27b0fec4aef4b0e2
Harry JongSeok LEE <lodossw@gmail.com>
2025-04-18 21:50:46 +0900

    초기화

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { BrowserRouter } from 'react-router'
-
-
-const queryClient = new QueryClient(); 
-
-
-
 createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    <ReactQueryDevtools initialIsOpen={false} buttonPosition={'bottom-right'}/>
-  </QueryClientProvider>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
